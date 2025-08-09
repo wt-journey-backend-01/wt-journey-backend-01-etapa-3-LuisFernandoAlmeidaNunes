fe31ece59ef47701bc2dc844929dada2bac4615e
luis <luis_jna@outlook.com>
2025-08-09 11:52:23 -0300

    fix: router

--- routes/agentesRoutes.js
+++ routes/agentesRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const agentesController = require('../controllersController');
+const agentesController = require('../controllers/agentesController');
 
 router.get('', agentesController.getAllAgentes);
 
--- routes/casosRoutes.js
+++ routes/casosRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const casosController = require('../controllersController');
+const casosController = require('../controllers/casosController');
 
 // define a rota para /agentes usando o método GET
 
